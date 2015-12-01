d7bfcfc82514fd1f07daf1716ca8ff1e7ffdc8c2
yuta yokoi <dezirus.novajn.iojn@gmail.com>
2015-12-02 01:06:27 +0900

    php artisan migrate

---
+++ database/migrations/2015_12_02_004914_create_articles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $tables) {
+            $tables->increments('id');
+            $tables->string('title');
+            $tables->text('body');
+            $tables->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('articles');
+    }
+}

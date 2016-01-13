3fbe093f8cd1ef7ee726541d3c94e9c22cf56e9f
yuta yokoi <dezirus.novajn.iojn@gmail.com>
2016-01-13 18:44:29 +0900

    Whoops 導入

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,6 +48,17 @@ class Handler extends ExceptionHandler
             $e = new NotFoundHttpException($e->getMessage(), $e);
         }
 
+        if (config('app.debug')) {
+            $whoops = new \Whoops\Run;
+            $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
+
+            return new \Illuminate\Http\Response(
+                $whoops->handleException($e),
+                $e->getStatusCode(),
+                $e->getHeaders()
+            );
+        }
+
         return parent::render($request, $e);
     }
 }
